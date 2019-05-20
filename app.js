26652cf7249210f4fd53d92436518965412946c3
Diana <coelhodiana04@gmail.com>
2019-05-19 21:57:56 -0300

    Primeiro commit do projeto

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+    res.render("search");
+});
 
 app.get('/results', (req, res) => {
-    request("http://www.omdbapi.com/?s=star&apikey=thewdb", (error, response, body)=> {
+    const final = "&apikey=thewdb";
+    let search = req.query.keyword;
+    const url = "http://www.omdbapi.com/?s=" + search + final;
+
+    request(url, (error, response, body)=> {
         if(!error && response.statusCode == 200){
-            let results = JSON.parse(body)
-            res.send(results);
+            let data = JSON.parse(body)
+            res.render("results", {data: data});
         }
     });
   });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log('Servidor rodando nice n easy!');
   });
